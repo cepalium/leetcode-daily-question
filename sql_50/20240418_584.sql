edc6695a44c33a419455b1569afb7b097324d998
Tuan Nguyen <tuan15096@gmail.com>
2024-04-18 15:10:04 +0700

    add sql_50

---
+++ sql_50/20240418_584.sql
@@ -0,0 +1,26 @@
+/*
+584. Find Customer Referee (Easy)
+
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+select name
+from Customer
+where coalesce(referee_id, 0) != 2;
